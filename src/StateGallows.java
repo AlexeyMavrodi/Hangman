58f00e84705fe7ed7b1059597466d9306feff1f6
Alexey <obushko02@mail.ru>
2026-03-10 00:23:02 +0300

    feat: add StateGallows to describe states

---
+++ src/StateGallows.java
@@ -0,0 +1,66 @@
+public enum StateGallows {
+    ONE_MISTAKE("━━━━━━━━━━━━━━━━\n" +
+                "┃              ┋\n" +
+                "┃              ┋\n" +
+                "┃              O\n" +
+                "┃               \n" +
+                "┃               \n" +
+                "┃               \n" +
+                "┃               \n" +
+                "━━━             \n"),
+    TWO_MISTAKES("━━━━━━━━━━━━━━━━\n" +
+                 "┃              ┋\n" +
+                 "┃              ┋\n" +
+                 "┃              O\n" +
+                 "┃              0\n" +
+                 "┃               \n" +
+                 "┃               \n" +
+                 "┃               \n" +
+                 "━━━             \n"),
+    THREE_MISTAKES("━━━━━━━━━━━━━━━━\n" +
+                   "┃              ┋\n" +
+                   "┃              ┋\n" +
+                   "┃              O\n" +
+                   "┃             /0\n" +
+                   "┃               \n" +
+                   "┃               \n" +
+                   "┃               \n" +
+                   "━━━             \n"),
+    FOUR_MISTAKES("━━━━━━━━━━━━━━━━\n" +
+                  "┃              ┋\n" +
+                  "┃              ┋\n" +
+                  "┃              O\n" +
+                  "┃             /0\\\n" +
+                  "┃               \n" +
+                  "┃               \n" +
+                  "┃               \n" +
+                  "━━━             \n"),
+    FIVE_MISTAKES("━━━━━━━━━━━━━━━━\n" +
+                  "┃              ┋\n" +
+                  "┃              ┋\n" +
+                  "┃              O\n" +
+                  "┃             /0\\\n" +
+                  "┃             / \n" +
+                  "┃               \n" +
+                  "┃               \n" +
+                  "━━━             \n"),
+    SIX_MISTAKES("━━━━━━━━━━━━━━━━\n" +
+                 "┃              ┋\n" +
+                 "┃              ┋\n" +
+                 "┃              O\n" +
+                 "┃             /0\\\n" +
+                 "┃             / \\\n" +
+                 "┃               \n" +
+                 "┃               \n" +
+                 "━━━             \n");
+
+    private final String state;
+
+    StateGallows(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
